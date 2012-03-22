e8104a4b009373ce4803d461e8f65fb0de6fd611
Olly Blake <blake.olly@gmail.com>
2012-03-22 12:51:26 +0000

    Revert "Everything commit"
    This reverts commit 47e5cc0ff8222083825800b9637f2eb0b91b87cd.

--- appvars.php
+++
@@ -1,4 +0,0 @@
-<?php
-    define('GW_UPLOADPATH', 'images/');
-	define('GW_MAXFILESIZE', 819200); // 800KB
-?>
--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-   // Define the database connection constants
-   define('DB_HOST', 'localhost');
-   define('DB_USER', 'root');
-   define('DB_PASSWORD', 'root');
-   define('DB_NAME', 'zombees');
-?>
