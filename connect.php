ba14d229f2f701d5382d7a7591f8de2aa94030a1
Olly Blake <blake.olly@gmail.com>
2012-03-27 16:47:59 +0100

    changed password to blank

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
    // Define the database connection constants
    define('DB_HOST', 'localhost');
    define('DB_USER', 'root');
-   define('DB_PASSWORD', 'root');
+   define('DB_PASSWORD', '');
    define('DB_NAME', 'zombees');
 ?>
